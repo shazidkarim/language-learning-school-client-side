a65ca57d4235d3ecd6bc97d88e220d0ac7d33f81
shazidkarim <shazid747@gmail.com>
2023-06-08 10:23:30 +0600

    navbar done

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer className="footer p-10 bg-base-200 text-base-content">
             <div>
                <img className='w-32 h-32 object-contain' src={logo} alt="" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p>Language Learning School<br />Providing reliable tech since 1992</p>
             </div>
             <div>
                 <span className="footer-title">Services</span>

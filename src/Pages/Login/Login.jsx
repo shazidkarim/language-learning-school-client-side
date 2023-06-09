f0ab6774a2d06bc07249984127ae55d79074518e
shazidkarim <shazid747@gmail.com>
2023-06-09 22:40:33 +0600

    private route created

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,40 +2,40 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
 const Login = () => {
-
     const { signIn } = useContext(AuthContext);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
+    const handleLogin = async (data) => {
+        try {
+            const { email, password } = data;
+            console.log(email, password);
+            const result = await signIn(email, password);
+            const user = result.user;
+            console.log(user);
+            reset();
+            Swal.fire({
+                title: 'Successfully log in',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__fadeOutUp'
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-        signIn(email, password)
-        .then(result => {
-                const user = result.user;
-                console.log(user);
-                form.reset();
-                Swal.fire({
-                    title: 'Successfully log in',
-                    showClass: {
-                      popup: 'animate__animated animate__fadeInDown'
-                    },
-                    hideClass: {
-                      popup: 'animate__animated animate__fadeOutUp'
-                    }
-                  })
-            })
-    }
     return (
         <>
-         <Helmet>
-            <title>Language learning | login</title>
-        </Helmet>
+            <Helmet>
+                <title>Language learning | login</title>
+            </Helmet>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
                     <div className=" w-1/2 mr-5">
@@ -44,18 +44,32 @@ const Login = () => {
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
                             <h1 className='text-5xl text-primary font-bold text-center'>Login</h1>
-                            <form onSubmit={handleLogin}>
+                            <form onSubmit={handleSubmit(handleLogin)}>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                                    <input
+                                        type="email"
+                                        {...register("email", { required: true })}
+                                        name='email'
+                                        placeholder="email"
+                                        className="input input-bordered"
+                                    />
+                                    {errors.email && <span className="text-red-600 font-serif">This field is required</span>}
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                    <input
+                                        type="password"
+                                        {...register("password", { required: true })}
+                                        name='password'
+                                        placeholder="password"
+                                        className="input input-bordered"
+                                    />
+                                    {errors.password && <span className="text-red-600 font-serif">This field is required</span>}
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            <h2>this is private route </h2>
-        </div>
-    );
+const PrivateRoute = ({children}) => {
+    const {user} = useContext(AuthContext);
+    if(user){
+        return children;
+    }
+    return <Navigate to={'/login'}></Navigate>;
 };
 
 export default PrivateRoute;
